59a4875499ca829855fd822c4d309bae3873aea0
Shushrusha <94350776+Scribblingcheese@users.noreply.github.com>
2024-02-13 19:47:22 +0545

    Update app.py

--- app.py
+++ app.py
@@ -29,10 +29,10 @@ def update_value():
     month_to_no = {1:'JAN',2:'FEB',3:'MAR',4:'APR',5:'MAY',6:'JUN',7:'JUL',8:'AUG',9:'SEP',10:'OCT',11:'NOV',12:'DEC'}
     
     # Fetch the service account key JSON file contents
-    cred = credentials.Certificate('testing64jc-firebase-adminsdk-lz1q6-8a9c3c25c2.json')
+    #cred = credentials.Certificate('testing64jc-firebase-adminsdk-lz1q6-8a9c3c25c2.json')
 
     # Initialize the app with a service account, granting admin privileges
-    firebase_admin.initialize_app(cred, {'databaseURL': "https://krishi-6c36b-default-rtdb.firebaseio.com"})
+    #firebase_admin.initialize_app(cred, {'databaseURL': "https://krishi-6c36b-default-rtdb.firebaseio.com"})
     firebase_admin.get_app()
 
     ok = True
